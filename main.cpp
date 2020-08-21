aec07f8405ea493749cdf4f5a6b1709f8f59ea89
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-08-21 15:12:00 -0400

    Add required toolchain files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
+    float x = 5.4;
+    float y = 6.3;
+    std::cout << "Sum of two numbers is: " << x + y << std::endl;
     std::cout << "Hello, world!" << std::endl;
 }
 
