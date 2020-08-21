6cbf7b78fdfe9be620d95de87e0173ebcb2850a6
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-08-21 10:55:15 -0400

    Add build-cxx.sh

---
+++ build-libcxx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+target=arm-frc2020-linux-gnueabi
+compiler_root=`pwd`/frc2020/roborio
+sysroot=$compiler_root/$target
+install_dir=`pwd`/libcxx
+
+rm -rf build-libcxx $install_dir
+mkdir build-libcxx $install_dir
+cd build-libcxx
+CC=clang CXX=clang++ cmake \
+    -DCMAKE_INSTALL_PREFIX="$install_dir" \
+    -DCMAKE_SYSTEM_PROCESSOR=arm \
+    -DCMAKE_SYSTEM_NAME=Linux \
+    -DCMAKE_CROSSCOMPILING=True \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_LIBRARY_PATH="$sysroot/usr/lib/" \
+    -DCMAKE_CXX_FLAGS="-target $target -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=softfp --sysroot=$sysroot -gcc-toolchain $sysroot -B $compiler_root/bin -B $sysroot/usr/lib/$target/7.3.0" \
+    -DCMAKE_EXE_LINKER_FLAGS="-L$sysroot/usr/lib/$target/7.3.0" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-L$sysroot/usr/lib/$target/7.3.0" \
+    -DLLVM_PATH=../llvm-project/llvm \
+    -DLIBCXX_CXX_ABI=libstdc++ \
+    -DLIBCXX_CXX_ABI_INCLUDE_PATHS="$sysroot/usr/include/c++/7.3.0;$sysroot/usr/include/c++/7.3.0/$target" \
+    -DLIBCXX_CXX_ABI_LIBRARY_PATH="$sysroot/usr/lib/libstdc++.so" \
+    -GNinja \
+    ../llvm-project/libcxx
+ninja && ninja install
+cp $sysroot/usr/include/c++/7.3.0/bits/cxxabi_init_exception.h $install_dir/include/c++/v1/bits/
