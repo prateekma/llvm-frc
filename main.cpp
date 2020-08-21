5ac60335aa6453a1b3f2ca1a68da889a84f67705
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-08-21 15:49:44 -0400

    Add C++20 support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     float x = 5.4;
     float y = 6.3;
+    double result = std::lerp(1.0, 2.0, 0.5);
+    std::cout << "Lerp result is: " << result << std::endl;
     std::cout << "Sum of two numbers is: " << x + y << std::endl;
     std::cout << "Hello, world!" << std::endl;
 }
